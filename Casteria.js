0398827bc6d5b83caf431204f8e80430485e1493
kouya-kobayashi <k.kobayashi.cingroup2012@gmail.com>
2024-09-27 00:17:58 +0900

    修正しました。

--- Casteria.js
+++ Casteria.js
@@ -1,6 +1,13 @@
 $('.slider').slick({
-    autoplay: true,
-    dots: true,
-    slidesToShow: 2,
-    autoplaySpeed: 900,
-  });
+  autoplay: true,
+  dots: true,
+  slidesToShow: 2,
+  autoplaySpeed: 900,
+});
+
+$('.accordion-header').click(function() {
+  $('.accordion-header').not(this).next().slideUp();
+  $('.accordion-header').not(this).removeClass('active');
+  $(this).next().slideToggle();
+  $(this).toggleClass('active');
+});
